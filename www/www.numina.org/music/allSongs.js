9a64572b835e368bac1a84785907c4536c2e3943
Johnafriedman <john@innovationsforlearning.org>
2022-10-17 15:13:38 -0500

    adding song data

---
+++ www/www.numina.org/music/allSongs.js
@@ -0,0 +1,161 @@
+const songs = [
+    {
+        title : "A Groovy Little Xmas",
+
+        description : "",
+
+        sourceId : "152V4zESM7M6QT4FAhNpr24chgUsNrrWY"
+    },
+
+    {
+        title : "My Girl",
+
+        description : "I recorded the Temptations' My Girl" +
+            " as a wedding present for my friends Amy & Dave K.<br/>" +
+            "Dave and I used to play it in our band Biskets.",
+
+        sourceId : "14_EcV84HAfocCoqscXL4_ngf-cTWMBgV"
+    },
+
+    {
+        title : "River",
+
+        description : "I wrote this for and performed at my brother-in-law Don's memorial gathering.<br/> He was a great guitarist.",
+
+        sourceId : "14VY47Gs-LTyu4MoLzGb3uEV_sV3UySnQ"
+    },
+
+    {
+        title : "Kate's Great",
+
+        description : "For my sister's birthday",
+
+        sourceId : "14_raX2axn_Uy9V5TLzJSN2W__cK09VgX"
+    },
+
+    {
+        title : "WeddingAmble",
+
+        description : "Recorded my arrangement of Felix Mendelssohn's Wedding March from A Midsummer Night's Dream.<br/> It was played at my friends' Scott and Edie's wedding.",
+
+        sourceId : "15eG04oN6ECdICWm-FOSw5DMijMpoWK4D"
+    },
+
+    {
+        title : "Via par Avion",
+
+        description : "Recorded this for my daughter Sylvia when she was away in France.",
+
+        sourceId : "15lLDe_j1YuVyOaASciKBq7HhnLes3Eak"
+    },
+
+    {
+        title : "The Way You Sound Tonight",
+
+        description : "I recorded this for my brother Paul's birthday the first year of the pandemic <br/> because he loved a party and there were no parties.",
+
+        sourceId : "1GiQBfhvQYDhlVCBVjt5WVLreqCDuCexA"
+    },
+
+    {
+        title : "Simple Tune",
+
+        description : "a song fragment ",
+
+        sourceId : "15lfGhj7-YoEbFGsZS1sW9qYmdZCXWiSV"
+    },
+
+    {
+        title : "Riddle Song",
+
+        description : "My dad used to sing this traditional lullaby to mm mom when they were courting.<br/> I recorded it for their anniversary.",
+
+        sourceId : "14Vaj0rqOgbAwDDmQzBCXJJ2gL7QH6nny"
+    },
+
+    {
+        title : "Going Down",
+
+        description : "simple bluesy tune with a decending key change",
+
+        sourceId : "15T0o8a8q_mvijXrJDiueAfZ6l7U2Fr2Y"
+    },
+
+    {
+        title : "funk",
+
+        description : "jamming over Garage Band",
+
+        sourceId : "15YWizDPMP8U3EfPu9iKKbrwxs9GzrdgN"
+    },
+
+    {
+        title : "EDAF",
+
+        description : "For my son Elias' birthday. The four note figure are his initials.<br/>" +
+            "Recorded it with my phone earphone/mic inside the sound hole of my guitar.<br/>" +
+            "I intentionally kept the sound of it knocking on the body. It also includes <br/>" +
+            "some radiator sounds.",
+
+        sourceId : "1BjKLbvuNQXgSnZOEx6hHgdEP97isJC6R"
+    },
+
+    {
+        title : "DML7",
+
+        description : "Long acoustic improvisation for my friend Dave M's birthday",
+
+        sourceId : "14nEX-Hw9E5U5HetYGoK8K_JAeOQixDNf"
+    },
+
+    {
+        title : "Closer Walk",
+
+        description : "My dad's favorite spiritual, recorded for his birthday",
+
+        sourceId : "14tB_arxUKCOaie3YwPHBBlztBQNSg0Hj"
+    },
+
+    {
+        title : "practice",
+
+        description : "a song fragment to practice lead against",
+
+        sourceId : "15uM833QKhd-F1-WFwua5AJjkSlct6XxX"
+    },
+
+    {
+        title : "Bb12312312",
+
+        description : "a short fragment",
+
+        sourceId : "14u6TCNLRo9BB-UoYO0jkbJggM4fqFBBB"
+    },
+
+        {
+        title : "9_14_05",
+
+        description : "long improvisation frequently puts me to sleep",
+
+        sourceId : "14jZrAQuOwgYANvLuxDGRJ0z1iHCs2fFv"
+    },
+
+    {
+        title : "5verb",
+
+        description : "some electronics",
+
+        sourceId : "14jc1pBr-RTyB9UmIkkFpd9UqQFHkf1XV"
+    },
+
+    {
+        title : "Net 20130609",
+
+        description : "annoying improvisation over a right hand arpeggio exercise",
+
+        sourceId : "160CL1N1z2LeG9N8Wm9hSn5FpqNLvj-SU"
+    }
+]
+
+
+export default songs;
