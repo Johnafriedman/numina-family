fc5c1e572a9da0539079089db1e653bfa45f876e
Johnafriedman <john@innovationsforlearning.org>
2023-01-04 12:39:54 -0600

    fixing iPad not recognized

--- www/www.numina.org/asteroids/ipad.js
+++ www/www.numina.org/asteroids/ipad.js
@@ -2,7 +2,7 @@ var ipad = navigator.userAgent.match(/iPad/i) != null;
 
 const deviceType = () => {
   const ua = navigator.userAgent;
-  if (/(tablet|ipad|playbook|silk)|(android(?!.*mobi))/i.test(ua)) {
+  if (/(tablet|ipad|iPad|playbook|silk)|(android(?!.*mobi))/i.test(ua)) {
     return "tablet";
   }
   else if (/Mobile|Android|iP(hone|od)|IEMobile|BlackBerry|Kindle|Silk-Accelerated|(hpw|web)OS|Opera M(obi|ini)/.test(ua)) {
