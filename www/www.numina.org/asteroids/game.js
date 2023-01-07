4be7f2b82e23050fef8b0be71ac0a8252b59b7f3
Johnafriedman <john@innovationsforlearning.org>
2023-01-07 15:50:56 -0600

    customizing title

--- www/www.numina.org/asteroids/game.js
+++ www/www.numina.org/asteroids/game.js
@@ -917,7 +917,8 @@ Game = {
       this.state = 'waiting';
     },
     waiting: function () {
-      Text.renderText(window.ipad ? 'Touch Screen to Start' : 'Press Space to Start', 36, Game.canvasWidth/2 - 270, Game.canvasHeight/2);
+      Text.renderText(window.ipad ? 'LUKAS VS.the ASTEROIDS' : 'Press Space to Start', 36, Game.canvasWidth/2 - 270, Game.canvasHeight/2);
+      Text.renderText(window.ipad ? 'Touch Screen to Start' : 'Press Space to Start', 36, Game.canvasWidth/2 - 270, Game.canvasHeight/2+46);
       if (KEY_STATUS.space || window.gameStart) {
         KEY_STATUS.space = false; // hack so we don't shoot right away
         window.gameStart = false;
